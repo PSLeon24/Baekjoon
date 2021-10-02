4751df276d81166dd4620f9a193c670b56697041
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2021-10-02 21:00:39 +0900

    Create 11179.py

---
+++ 11179.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+n = bin(n)
+print(n[2:])
+n = list(n)
+del n[:2]
+tmp = []
+for i in range(1, len(n)+1):
+    tmp.append(n[-i])
+
+#tmp.insert(0, '0')
+#tmp.insert(1, 'b')
+#tmp = list(map(int, tmp))
+j = "".join(tmp)
+j = int(j, 2)
+print(j)
