fbfee6aeb59310bef08211f25b1cb22887c9493e
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2021-10-03 02:12:03 +0900

    Update 2609.py
    Add GCD, LCM algorithm

--- 2609.py
+++ 2609.py
@@ -1,4 +1,22 @@
+'''
 from math import gcd, lcm
+
+a, b = map(int, input().split())
+
+print(gcd(a, b))
+print(lcm(a, b))
+'''
+
+
+def gcd(num1, num2):
+    while num2 != 0:
+        num1, num2 = num2, num1 % num2
+    return num1
+
+
+def lcm(num1, num2):
+    return num1 * num2 // gcd(num1, num2)
+
 
 a, b = map(int, input().split())
 
