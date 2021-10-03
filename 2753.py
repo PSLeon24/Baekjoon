f99a584368560efd9a5aebe1176ca126007748c8
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2021-10-04 01:50:02 +0900

    new solve 2753

--- 2753.py
+++ 2753.py
@@ -1,3 +1,18 @@
+from calendar import isleap
+
+
+def leaf(year):
+    bool_year = isleap(year)
+    if bool_year == False:
+        return 0
+    else:
+        return 1
+
+
+years = int(input())
+print(leaf(years))
+
+'''
 # Step 1. Input Year
 year = int(input())
 
@@ -6,3 +21,4 @@ if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
     print(1)
 else:
     print(0)
+'''
